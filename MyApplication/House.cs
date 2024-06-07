e8fbf173b464c392f3808e221cb4db5e602be6c8
Jose Puebla <pueblaji2003@gmail.com>
2024-06-07 14:51:01 -0500

    CAC-103 C# Overveiw

---
+++ MyApplication/House.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MyFirstApplication;
+
+/*you change change internal to public but it is not needed
+*/
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open");
+    }
+}
+
--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//puts local function to use
+//single line comment
+HouseExample(); 
+
+/*
+multi-line comment
+the below statement is a local functoin. Local functions
+are inside of members and are private by default. This means
+They are specific to that member and can only be called 
+inside that member. 
+*/
+void HouseExample() //declares local function
+{
+    //this inner part can be done without the outer function
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
